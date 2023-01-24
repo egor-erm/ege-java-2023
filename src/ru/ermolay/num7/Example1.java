916c7068a9908794169c21aa71f0230bca9fb89b
Ermolay <es.ermolaev2005@gmail.com>
2023-01-24 22:38:03 +0300

    Num 7 Update

---
+++ src/ru/ermolay/num7/Example1.java
@@ -0,0 +1,14 @@
+package ru.ermolay.num7;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        float w = (float)(600 * 600) / (300 * 300);
+
+        float c = (float)24 / 16;
+
+        float otv = (float)12 / (w * c);
+
+        System.out.println(otv);
+    }
+}
---
+++ src/ru/ermolay/num7/examples.md
@@ -0,0 +1,4 @@
+# Num7
+### Example1 - https://inf-ege.sdamgia.ru/problem?id=18557
+> Решаем задачку.
+> Ответ: 2
