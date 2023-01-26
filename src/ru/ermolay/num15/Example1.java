f1587ef00f573008234989869f6bd69b35a002a4
Ermolay <es.ermolaev2005@gmail.com>
2023-01-26 22:10:56 +0300

    Num 15 Update

---
+++ src/ru/ermolay/num15/Example1.java
@@ -0,0 +1,28 @@
+package ru.ermolay.num15;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        for (int a = 500; a > 0; a--) {
+            int k = 0;
+            for (int x = 0; x < 500; x++) {
+                for (int y = 0; y < 500; y++) {
+                    if ((parseBool(x <= 9) <= parseBool(x * x <= a)) && (parseBool(y * y <= a) <= parseBool(y <= 9))) {
+                        k++;
+                    }
+                }
+            }
+
+            if (k == 500 * 500) {
+                System.out.println(a);
+                return;
+            }
+        }
+    }
+
+    public static int parseBool(boolean b) {
+        if (b) return 1;
+
+        return 0;
+    }
+}
