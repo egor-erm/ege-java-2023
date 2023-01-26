c6d653581bcaa42cad18f9362f49e7e46c48d4eb
Ermolay <es.ermolaev2005@gmail.com>
2023-01-26 22:29:14 +0300

    Num 15 Optimization Update

--- src/ru/ermolay/num15/Example1.java
+++ src/ru/ermolay/num15/Example1.java
@@ -6,11 +6,17 @@ public class Example1 {
         for (int a = 500; a > 0; a--) {
             int k = 0;
             for (int x = 0; x < 500; x++) {
+                boolean br = false;
                 for (int y = 0; y < 500; y++) {
                     if ((parseBool(x <= 9) <= parseBool(x * x <= a)) && (parseBool(y * y <= a) <= parseBool(y <= 9))) {
                         k++;
+                    } else {
+                        br = true;
+                        break;
                     }
                 }
+
+                if (br) break;
             }
 
             if (k == 500 * 500) {
